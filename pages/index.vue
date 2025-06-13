aea33f17ba1edc30cf306b112ce9a527103b43bc
Ricardo Alegria <ralegria1212@gmail.com>
2025-06-13 14:12:51 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>Empieza a vender en internet, ya!</h1>
+    <p>Más de 1000 negocios usan STORES para potenciar sus ventas en Internet</p>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    background-color: #f3f3f3;
+    padding: 20px;
+    text-align: center;
+  }
+</style>
